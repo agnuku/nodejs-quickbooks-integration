a37b10a5821ce61d5cbb67e140f909ba0c47b3f8
agnuku <agnuku@users.noreply.github.com>
2023-06-29 03:22:24 +0000

    new server

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+// routes/index.js
+
+const express = require('express');
+const router = express.Router();
+const csrf = require('csrf');
+const tokens = new csrf();
+
+const { OAuthClient } = require('intuit-oauth');
+
+router.get('/', function(req, res) {
+    res.send('Welcome to QuickBooks connection sample app');
+});
+
+router.get('/connect', function(req, res) {
+    const authUri = req.oauthClient.authorizeUri({
+        scope: [OAuthClient.scopes.Accounting],
+        state: tokens.create(req.sessionID),
+    });
+    res.redirect(authUri);
+});
+
+router.get('/callback', function(req, res) {
+    const parseRedirect = req.url;
+
+    if (!tokens.verify(req.sessionID, req.query.state)) {
+        return res.json({error: 'Invalid state'});
+    }
+
+    req.oauthClient.createToken(parseRedirect)
+        .then(function(authResponse) {
+            req.session.authResponse = authResponse.json();
+            req.session.save(function(err) {
+                if(err) {
+                    console.log(err);
+                }
+                res.redirect('/');
+            });
+        })
+        .catch(function(e) {
+            console.error(e);
+        });
+});
+
+module.exports = router;
