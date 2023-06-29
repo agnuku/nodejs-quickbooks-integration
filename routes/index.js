ddc3ac9ca80e29d64cce00c7b4800fa9dd0bf99f
agnuku <agnuku@users.noreply.github.com>
2023-06-29 22:04:05 +0000

    #15

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,26 @@ const express = require('express');
 const router = express.Router();
 const csrf = require('csrf');
 const tokens = new csrf();
-const logger = require('../logger'); // import the logger
+const logger = require('../logger'); 
 
 const { OAuthClient } = require('intuit-oauth');
 
-function calculateStartDate() {
-    let date = new Date();
-    date.setFullYear(date.getFullYear() - 1);
-    return date;
-}
-
 router.get('/generalledger', function(req, res) {
     logger.info('GET /generalledger route hit');
-    if(!req.session || !req.session.authResponse) {
-        logger.warn('Session or auth response not available');
-        return res.json({ error: 'Session or auth response not available' });
+    
+    if(!req.session || !req.session.authResponse || !req.session.authResponse.realmId) {
+        logger.warn('Session, auth response, or realmId not available');
+        return res.json({ error: 'Session, auth response, or realmId not available' });
     }
+
     const oauthClient = req.oauthClient;
     const companyID = req.session.authResponse.realmId;
-    
+
     const startDate = '2022-01-01';
     const endDate = '2022-12-31';
 
     const url = `${oauthClient.environment == 'sandbox' ? 'https://sandbox-quickbooks.api.intuit.com' : 'https://quickbooks.api.intuit.com'}/v3/company/${companyID}/reports/GeneralLedger`;
-    
+
     const queryParameters = {
         start_date: startDate,
         end_date: endDate,
@@ -33,7 +29,7 @@ router.get('/generalledger', function(req, res) {
         source_account_type: 'Bank',
         minorversion: 65
     };
-    
+
     const requestUri = oauthClient.token.getToken().token_type + ' ' + oauthClient.token.getToken().access_token;
     const authHeaders = {
         headers: {
@@ -56,10 +52,12 @@ router.get('/generalledger', function(req, res) {
 
 router.get('/companyinfo', function(req, res) {
     logger.info('GET /companyinfo route hit');
-    if(!req.session || !req.session.authResponse) {
-        logger.warn('Session or auth response not available');
-        return res.json({ error: 'Session or auth response not available' });
+
+    if(!req.session || !req.session.authResponse || !req.session.authResponse.realmId) {
+        logger.warn('Session, auth response, or realmId not available');
+        return res.json({ error: 'Session, auth response, or realmId not available' });
     }
+
     const oauthClient = req.oauthClient;
     const companyID = req.session.authResponse.realmId;
 
@@ -85,22 +83,15 @@ router.get('/companyinfo', function(req, res) {
     });
 });
 
-
 router.get('/', function(req, res) {
     logger.info('GET / route hit');
-    res.send('Welcome to QuickBooks connection sample app');
+    res.send('Welcome to Quickbooks Integration');
 });
 
-router.get('/connect', function(req, res) {
-    logger.info('GET /connect route hit');
-    logger.debug("In /connect route, req.oauthClient clientId: " + req.oauthClient.clientId);
-    
-    const authUri = req.oauthClient.authorizeUri({
-        scope: ['com.intuit.quickbooks.accounting'],
-        state: tokens.create(req.sessionID),
-    });
-    logger.info('Redirecting to: ' + authUri);
-    res.redirect(authUri);
+router.get('/connect_to_quickbooks', function(req, res) {
+    logger.info('GET /connect_to_quickbooks route hit');
+    req.session.csrfToken = tokens.create(req.sessionID);
+    res.redirect(oauthClient.authorizeUri({scope:[OAuthClient.scopes.Accounting],state:req.session.csrfToken}));
 });
 
 router.get('/callback', function(req, res) {
@@ -116,19 +107,20 @@ router.get('/callback', function(req, res) {
     req.oauthClient.createToken(parseRedirect)
         .then(function(authResponse) {
             logger.debug('Token creation successful, saving session...');
-            req.session.authResponse = authResponse.getJson(); // Corrected line
+            logger.debug('Auth response: ', authResponse.getJson()); // Added this line to log auth response
+            req.session.authResponse = authResponse.getJson();
             req.session.save(function(err) {
-                if(err) {
-                    logger.error("Error occurred while saving session: " + err.message);
-                    return res.status(500).json({ error: 'Error during session saving' });
+                if (err) {
+                    logger.error('Session saving error: ', err);
+                    return res.json({ error: 'Could not save session' });
                 }
-                logger.info('Session saved successfully, redirecting to /');
-                res.redirect('/');
+                logger.info('Session saved successfully');
+                return res.redirect('/connected');
             });
         })
         .catch(function(e) {
-            logger.error("Error occurred while creating token: " + e.message);
-            res.status(500).json({ error: 'Error during token creation' });
+            logger.error('Token creation error: ', e);
+            return res.json({ error: 'Could not create token' });
         });
 });
 
