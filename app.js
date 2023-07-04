9d7801331a9157e9feef3e62df826cd51460c34d
agnuku <agnuku@users.noreply.github.com>
2023-07-04 08:57:19 +0000

    cors off

--- app.js
+++ app.js
@@ -52,17 +52,7 @@ client.connect().then(() => {
 
 let app = express();
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = `The CORS policy for this site does not allow access from the specified origin.`;
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true
-}));
+app.use(cors());
 
 
 const PORT = process.env.PORT || 5000;
